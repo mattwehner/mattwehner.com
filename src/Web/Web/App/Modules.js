d94ff98f8240c2fcda446fccac594d8ee5c9677c
Matt <mattwehnernz@gmail.com>
2016-09-11 23:12:10 +1200

    Started Angular App

---
+++ src/Web/Web/App/Modules.js
@@ -0,0 +1,9 @@
+﻿var mattwehnerApp = angular.module("mattwehnerApp", [
+    "mattwehnerControllers",
+    "mattwehnerRouting",
+]);
+
+var hourfleetControllers = angular.module("mattwehnerControllers", []);
+var hourfleetControllers = angular.module("mattwehnerRouting", [
+    "ui.router",
+]);
