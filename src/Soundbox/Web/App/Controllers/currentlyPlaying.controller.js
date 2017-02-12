5191b32ed28dcb5b27b30c4b08673cd4e3ea6d5f
Matt <mattwehnernz@gmail.com>
2017-02-12 13:13:04 +1300

    font awesome files

---
+++ src/Soundbox/Web/App/Controllers/currentlyPlaying.controller.js
@@ -0,0 +1,17 @@
+﻿soundboxControllers.controller('currentlyPlaying.controller',
+[
+    '$scope',
+    'ControllerRequestService',
+    function(
+        $scope,
+        ControllerRequestService
+    ) {
+        $scope.model = {
+            campaign: {},
+            tabSections: {},
+            pageElements: {
+                expanded: false,
+            }
+        };
+    }
+]);
